3caba3b1090da9703229a27f6ea1d7f5bd5f8f62
BOTCAHX <tioclkp02@gmail.com>
2024-12-09 07:02:52 +0700

    fix fetch version

--- version.js
+++ version.js
@@ -1,14 +1,18 @@
 const axios = require('axios');
-const package_v = require('./package.json'); 
+const package_v = require('./package.json');
 async function versionControl(version) {
     try {
-        const response = await axios.get(`https://registry.npmjs.com/-/v1/search?text=btch-gemini`);
+        const response = await axios.get('https://registry.npmjs.org/btch-gemini');
         const pack = response.data;
 
-        if (pack && pack.objects && pack.objects[0] && pack.objects[0].package.version !== version) {
+        const latestVersion = pack['dist-tags'].latest;
+
+        if (latestVersion !== version) {
             console.log(
-                `\x1b[38;5;215mYou are using an Outdated Version for btch-gemini, Run the \x1b[38;5;119m' npm i btch-gemini@${pack.objects[0].package.version} '\x1b[38;5;215m Command for the Current Version.\x1B[0m`
+                `\x1b[38;5;215mYou are using an Outdated Version for btch-gemini. Run the \x1b[38;5;119m'npm i btch-gemini@${latestVersion}' \x1b[38;5;215mCommand for the Latest Version.\x1B[0m`
             );
+        } else {
+            console.log(`\x1b[38;5;119mYou are using the Latest Version (${latestVersion}) of btch-gemini.\x1B[0m`);
         }
     } catch (er) {
         console.error('Version check failed:', er.message || er);
