a2d4a1ef0bf97ddd73c618b241a2a3ec83a6012c
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2025-04-03 21:52:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * @file index
  * @description The main module for importing functions from `src/main.js` and executing a version check from `version.js`.
  * @remarks
- * - Imports `gemini_chat`, `gemini_image`, `gemini_audio`, `gemini_video`, `gemini_history`, and `gemini_prompt` functions to make them available for use in other parts of the project.
+ * - Imports `gemini_chat`, `gemini_image`, `gemini_audio`, `gemini_video`, `gemini_history`, `gemini_imgedit`, and `gemini_prompt` functions to make them available for use in other parts of the project.
  * - Calls `versionControl` to check that the current project version is up to date with the latest version of `btch-gemini`.
  * - Retrieves the current project version from `package.json` to use in the version check.
  */
@@ -16,5 +16,6 @@ module.exports = {
     gemini_audio: require('./src/main').gemini_audio,
     gemini_video: require('./src/main').gemini_video,
     gemini_history: require('./src/main').gemini_history,
-    gemini_prompt: require('./src/main').gemini_prompt
+    gemini_prompt: require('./src/main').gemini_prompt,
+    gemini_imgedit: require('./src/main').gemini_imgedit
 };
